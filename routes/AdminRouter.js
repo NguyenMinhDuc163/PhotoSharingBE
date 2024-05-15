2a21fe9337a8c11676123d8d462a338fe74ebe17
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2024-05-15 22:30:31 +0700

    add

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../db/userModel");
+const admin = require('../auth/firebaseAdmin'); // Import Firebase Admin SDK
 const router = express.Router();
 
 const JWT_SECRET = 'your-secret-key';
-const SALT_ROUNDS = 10; 
+const SALT_ROUNDS = 10;
 
 router.post("/login", async (req, res) => {
   const { login_name, password } = req.body;
@@ -23,8 +24,6 @@ router.post("/login", async (req, res) => {
     res.status(500).send({ error: "Internal server error" });
   }
 });
-
-
 
 router.post("/register", async (req, res) => {
   const { login_name, password, first_name, last_name, location, description, occupation } = req.body;
@@ -54,9 +53,48 @@ router.post("/register", async (req, res) => {
   }
 });
 
-
 router.post("/logout", (req, res) => {
   res.send({ message: "Successfully logged out" });
+});
+
+// Xử lý đăng nhập qua Google và GitHub
+router.post("/login-google-github", async (req, res) => {
+  const { token, email, first_name, last_name, provider } = req.body;
+  console.log(token);
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    console.log("Decoded Token:", decodedToken); // Thêm log ở đây để kiểm tra
+
+    let user = await User.findOne({ email: decodedToken.email });
+    console.log("Existing User:", user); // Thêm log ở đây để kiểm tra
+
+    if (!user) {
+      // Tạo người dùng mới nếu không tồn tại
+      user = new User({
+        login_name: email,
+        password: '123456', // Lưu giá trị mặc định
+        first_name: decodedToken.name,
+        last_name: last_name,
+        location: '', // Thêm các thông tin khác nếu cần
+        description: '',
+        occupation: '',
+        email: email,
+        provider: provider
+      });
+      await user.save();
+      console.log("New user created:", user); // Thêm log ở đây để kiểm tra
+    } else {
+      console.log("User already exists:", user); // Thêm log ở đây để kiểm tra
+    }
+
+    // Tạo JWT cho ứng dụng của bạn
+    const jwtToken = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+    res.send({ token: jwtToken, user });
+  } catch (error) {
+    console.error("Error verifying token or saving user:", error); // Thêm log lỗi ở đây
+    res.status(401).send({ error: "Invalid token" });
+  }
 });
 
 module.exports = router;
